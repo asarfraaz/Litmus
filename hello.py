17b90b19e7c9da2dad0ab9e663b75e9b92341e12
Sarfraaz Ahmed <sarfraaz@gmail.com>
2026-09-25 16:08:17 +0530

    script to run with uv

---
+++ hello.py
@@ -0,0 +1,14 @@
+import sys                                                                                            
+from datetime import datetime
+from rich import print
+import numpy
+
+print(sys.version)
+
+print("Hello", "World")
+
+val = 10
+print(f'{val = }')
+
+now = datetime.today()
+print("The time now is:", now)
